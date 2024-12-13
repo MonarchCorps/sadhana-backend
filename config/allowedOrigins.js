10cd8318d3553cc45e22b80892d23f69064956af
Monarch Corps <monarchcorps01@gmail.com>
2024-12-13 22:16:02 +0100

    Update code, not sure it's working

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
     'https://sadhana-backend.vercel.app',
     'https://sadhana-frontend.vercel.app',
-    "https://vercel.live/_next-live/feedback/feedback.js"
+    "https://vercel.live"
 ]
 module.exports = allowedOrigins
