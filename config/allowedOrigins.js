b9085c30ff732ef710240eaaf34e5ff2349db185
Monarch Corps <monarchcorps01@gmail.com>
2024-12-13 22:01:59 +0100

    Added a new origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
     'https://sadhana-backend.vercel.app',
-    'https://sadhana-frontend.vercel.app'
+    'https://sadhana-frontend.vercel.app',
+    "https://vercel.live/_next-live/feedback/feedback.js"
 ]
 module.exports = allowedOrigins
