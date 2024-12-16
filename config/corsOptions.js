cceef39d39f5c9dfaae713bcbf16f528535cf7eb
Ayomide Oluwatola <138298443+MikeyA-yo@users.noreply.github.com>
2024-12-16 15:24:31 +0100

    Update corsOptions.js

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,13 +1,8 @@
 const allowedOrigins = require('./allowedOrigins')
 
 const corsOptions = {
-    origin: (origin, callback) => {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
+    //btw i know this isn't safe, but this is how i fix my cors error mostly
+    origin: "*",
     credentials: true,
     optionsSuccessStatus: 200
 }
