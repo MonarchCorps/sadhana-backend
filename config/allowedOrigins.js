c4a541d0b8a80289fa1a29248a39c4c798863a7a
Monarch Corps <monarchcorps01@gmail.com>
2024-12-16 14:48:50 +0100

    Updated cors configuration

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-    'https://sadhana-backend.vercel.app/',
-    'https://sadhana-frontend.vercel.app/'
+    'https://sadhana-backend.vercel.app',
+    'https://sadhana-frontend.vercel.app'
 ]
 module.exports = allowedOrigins
